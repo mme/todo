a557d8fdcbda8a7cad397ea9075991f6347fdb55
Markus Ecker <markus.ecker@gmail.com>
2024-03-12 15:38:03 +0100

    Finalize first version

---
+++ src/app/api/copilotkit/route.ts
@@ -0,0 +1,7 @@
+import { CopilotBackend, OpenAIAdapter } from "@copilotkit/backend";
+
+export async function POST(req: Request): Promise<Response> {
+  const copilotKit = new CopilotBackend({});
+
+  return copilotKit.response(req, new OpenAIAdapter());
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,320 @@
-import Image from "next/image";
+"use client";
+
+/**
+ *
+ * 1) Import the CopilotKit components
+ *
+ **/
+
+import {
+  CopilotKit,
+  useCopilotAction,
+  useMakeCopilotReadable,
+} from "@copilotkit/react-core";
+import { CopilotPopup } from "@copilotkit/react-ui";
+import "@copilotkit/react-ui/styles.css";
+
+import { nanoid } from "nanoid";
+import { useState } from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="border rounded-md max-w-2xl mx-auto p-4">
+      <h1 className="text-2xl font-bold ">Hello CopilotKit 🪁</h1>
+      <h2 className="text-base font-base mb-4">Todo List Example</h2>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      {/**
+       *
+       * 2) Wrap the TodoList component with CopilotKit
+       *
+       **/}
+
+      <CopilotKit url="/api/copilotkit/">
+        <TodoList />
+
+        {/**
+         *
+         * 3) Add the CopilotPopup component to get the chat
+         *
+         */}
+
+        <CopilotPopup
+          instructions={
+            "Help the user manage a todo list. If the user provides a high level goal, " +
+            "break it down into a few specific tasks and add them to the list"
+          }
+          defaultOpen={true}
+          labels={{
+            title: "Todo List Copilot",
+            initial: "Hi you! 👋 I can help you manage your todo list.",
+          }}
+          clickOutsideToClose={false}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </CopilotKit>
+    </div>
   );
 }
+
+const TodoList: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [input, setInput] = useState("");
+
+  /**
+   *
+   * 4) make the users todo list available with useMakeCopilotReadable
+   *
+   **/
+  useMakeCopilotReadable(
+    "This is the user's todo list: " + JSON.stringify(todos)
+  );
+
+  /**
+   *
+   * 5) Add the useCopilotAction to enable the copilot to interact with the todo list
+   *
+   **/
+
+  useCopilotAction({
+    name: "updateTodoList",
+    description: "Update the users todo list",
+    parameters: [
+      {
+        name: "items",
+        type: "object[]",
+        description: "The new and updated todo list items.",
+        attributes: [
+          {
+            name: "id",
+            type: "string",
+            description:
+              "The id of the todo item. When creating a new todo item, just make up a new id.",
+          },
+          {
+            name: "text",
+            type: "string",
+            description: "The text of the todo item.",
+          },
+          {
+            name: "isCompleted",
+            type: "boolean",
+            description: "The completion status of the todo item.",
+          },
+          {
+            name: "assignedTo",
+            type: "string",
+            description:
+              "The person assigned to the todo item. If you don't know, assign it to 'YOU'.",
+            required: true,
+          },
+        ],
+      },
+    ],
+    handler: ({ items }) => {
+      console.log(items);
+      const newTodos = [...todos];
+      for (const item of items) {
+        const existingItemIndex = newTodos.findIndex(
+          (todo) => todo.id === item.id
+        );
+        if (existingItemIndex !== -1) {
+          newTodos[existingItemIndex] = item;
+        } else {
+          newTodos.push(item);
+        }
+      }
+      setTodos(newTodos);
+    },
+    render: "Updating the todo list...",
+  });
+
+  /**
+   *
+   * 5) Add another useCopilotAction to enable the copilot to delete a todo item
+   *
+   **/
+  useCopilotAction({
+    name: "deleteTodo",
+    description: "Delete a todo item",
+    parameters: [
+      {
+        name: "id",
+        type: "string",
+        description: "The id of the todo item to delete.",
+      },
+    ],
+    handler: ({ id }) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    render: "Deleting a todo item...",
+  });
+
+  const addTodo = () => {
+    if (input.trim() !== "") {
+      // Check if input is not just whitespace
+      const newTodo: Todo = {
+        id: nanoid(),
+        text: input.trim(), // Trim whitespace
+        isCompleted: false,
+      };
+      setTodos([...todos, newTodo]);
+      setInput(""); // Reset input field
+    }
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      addTodo();
+    }
+  };
+
+  const toggleComplete = (id: string) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id: string) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const assignPerson = (id: string, person: string | null) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id
+          ? { ...todo, assignedTo: person ? person : undefined }
+          : todo
+      )
+    );
+  };
+
+  return (
+    <div>
+      <div className="flex mb-4">
+        <input
+          className="border rounded-md p-2 flex-1 mr-2"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={handleKeyPress} // Add this to handle the Enter key press
+        />
+        <button
+          className="bg-blue-500 rounded-md p-2 text-white"
+          onClick={addTodo}
+        >
+          Add Todo
+        </button>
+      </div>
+      {todos.length > 0 && (
+        <div className="border rounded-lg">
+          {todos.map((todo, index) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              toggleComplete={toggleComplete}
+              deleteTodo={deleteTodo}
+              assignPerson={assignPerson}
+              hasBorder={index !== todos.length - 1}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export interface Todo {
+  id: string;
+  text: string;
+  isCompleted: boolean;
+  assignedTo?: string;
+}
+
+interface TodoItemProps {
+  todo: Todo;
+  toggleComplete: (id: string) => void;
+  deleteTodo: (id: string) => void;
+  assignPerson: (id: string, person: string | null) => void;
+  hasBorder?: boolean;
+}
+
+const TodoItem: React.FC<TodoItemProps> = ({
+  todo,
+  toggleComplete,
+  deleteTodo,
+  assignPerson,
+  hasBorder,
+}) => {
+  return (
+    <div
+      className={
+        "flex items-center justify-between px-4 py-2 group" +
+        (hasBorder ? " border-b" : "")
+      }
+    >
+      <div className="flex items-center">
+        <input
+          className="h-5 w-5 text-blue-500"
+          type="checkbox"
+          checked={todo.isCompleted}
+          onChange={() => toggleComplete(todo.id)}
+        />
+        <span
+          className={`ml-2 text-sm ${
+            todo.isCompleted ? "text-gray-500 line-through" : "text-gray-900"
+          }`}
+        >
+          {todo.assignedTo && (
+            <span className="border rounded-md text-xs py-[2px] px-1 mr-2  border-purple-700 uppercase bg-purple-400 text-black font-medium">
+              {todo.assignedTo}
+            </span>
+          )}
+          {todo.text}
+        </span>
+      </div>
+      <div>
+        <button
+          onClick={() => deleteTodo(todo.id)}
+          className="text-red-500 opacity-0 group-hover:opacity-100 transition-opacity duration-200"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+            />
+          </svg>
+        </button>
+        <button
+          onClick={() => {
+            const name = prompt("Assign person to this task:");
+            assignPerson(todo.id, name);
+          }}
+          className="ml-2 text-blue-500 opacity-0 group-hover:opacity-100 transition-opacity duration-200"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
